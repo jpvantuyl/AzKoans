962c68a1fa67f2ced2c1a0f9a2ed8869fe849a0e
John Van Tuyl <john.vantuyl@pemco.com>
2023-07-12 08:21:13 -0700

    sketch out hub and spoke and front door

---
+++ AzKoans/4-infrastructure/44-network/442-AboutFrontDoor.Tests.ps1
@@ -0,0 +1,35 @@
+Describe 'Function App' {
+    BeforeAll {
+        $rg = "$prefix-$num-$uniqueHash"
+        $st = "$prefix$($num)st$uniqueHash"
+        $fn = "$($prefix)-$num-fn-$uniqueHash"
+        $splat = @{
+            rg           = $rg
+            templateFile = "$PSScriptRoot\$num.bicep"
+            parameters   = @{
+                storageAccountName = $st
+                functionAppName    = $fn
+                tags               = $tags
+            }
+        }
+        Contemplate-AzResources @splat
+        $functionApp = Get-AzFunctionApp -ResourceGroupName $rg -Name $fn
+    }
+
+    It 'is in a good state' {
+        $functionApp.Status | Should -Be "Running"
+    }
+        
+    It 'renders a page' {
+        $response = Invoke-WebRequest -Uri "https://$fn.azurewebsites.net"
+        $response.StatusCode | Should -Be 200
+        $response.Content | Should -BeLike "*Azure Functions is an event-based serverless compute experience to accelerate your development*"
+    }
+
+    AfterAll {
+        $destroy = $false
+        if ($destroy) {
+            Get-AzResourceGroup -Name $rg | Remove-AzResourceGroup -Force -Verbose
+        }
+    }
+}
